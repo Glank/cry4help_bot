7f3a99d58ff282f5d8e14335bdf85f301955e8b6
Ernest Kirstein <glank314@gmail.com>
2022-02-18 06:35:54 -0500

    Implemented token clustering.

--- cry4help/scraping.py
+++ cry4help/scraping.py
@@ -23,23 +23,45 @@ class FloatPattern(NumericPattern):
     return float(match[1])
 
 class UnitPattern(NumericPattern):
-  def __init__(self, regex, unit):
-    NumericPattern.__init__(self, regex, 'unit')
+  def _get_regex(unit_regex):
+    return r'(?:[^a-z]|^)'+unit_regex+r'(?:[^a-z]|$)'
+  def __init__(self, unit_regex, unit):
+    NumericPattern.__init__(self, UnitPattern._get_regex(unit_regex), 'unit')
     self.unit = unit
   def value(self, match):
     return self.unit
+
+class KeywordPattern(NumericPattern):
+  def _get_regex(keyword_regex):
+    return r'(?:[^a-z]|^)'+keyword_regex+r'(?:[^a-z]|$)'
+  def __init__(self, keyword_regex, group):
+    NumericPattern.__init__(self, KeywordPattern._get_regex(keyword_regex), 'keyword')
+    self.group = group 
+  def value(self, match):
+    return self.group
     
 _NUMBER_PATTERNS = [
   FloatPattern(r'(\d+\.?\d*)'),
   FloatPattern(r'(\.\d+)'),
 ]
 _UNIT_PATTERNS = [
-  UnitPattern(r'(?:[^a-z]|^)(lbs?|pounds?)(?:[^a-z]|$)', 'lbs'),
-  UnitPattern(r'(?:[^a-z]|^)(kgs?|kilo|k)(?:[^a-z]|$)', 'kg'),
-  UnitPattern(r'(?:[^a-z]|^)(st|stones?)(?:[^a-z]|$)', 'st'),
-  UnitPattern(r'(?:[^a-z]|^)(bmi)(?:[^a-z]|$)', 'bmi'),
+  UnitPattern(r'(lbs?|pounds?)', 'lbs'),
+  UnitPattern(r'(kgs?|kilo|k)', 'kg'),
+  UnitPattern(r'(st|stones?)', 'st'),
+  UnitPattern(r'(bmi)', 'bmi'),
+  UnitPattern(r'(inches|"|in)', 'in'),
+  UnitPattern(r"(feet|'|ft)", 'ft'),
+  UnitPattern(r'(cm)', 'cm'),
+  UnitPattern(r'(m)', 'm'),
 ]
-_PATTERNS = _NUMBER_PATTERNS+_UNIT_PATTERNS
+_KEYWORD_PATTERNS = [
+  KeywordPattern(r'(down|lost|so far|total)', 'delta'),
+  KeywordPattern(r'(gw|goal)', 'goal'),
+  KeywordPattern(r'(cw|current|currently|stuck at)', 'current'),
+  KeywordPattern(r'(weigh)', 'weight'),
+  KeywordPattern(r'(tall)', 'height'),
+]
+_PATTERNS = _NUMBER_PATTERNS+_UNIT_PATTERNS+_KEYWORD_PATTERNS
 
 def scrape_numeric(string):
   """
@@ -47,7 +69,7 @@ def scrape_numeric(string):
   Returns a list of tokens, like this:
   return [
     {
-      "type": "number", # or unit
+      "type": "number", # or "unit" or "keyword"
       "value": 3.14,
       "start": 4,  # index in string of start of pattern match
       "end": 12, # index of last char of pattern matched
@@ -83,3 +105,103 @@ def scrape_numeric(string):
     offset += end
   return tokens
 
+def _token_dist(t1, t2):
+  return min(t1['end'], t2['end']) - min(t1['start'], t2['start'])
+
+class StickyRule:
+  def __init__(self, name):
+    self.name = name
+  def sticks(self, cluster):
+    raise NotImplementedError()
+
+class BasicStickyRule(StickyRule):
+  def __init__(self, name, units, keyword_groups, max_dist=250):
+    StickyRule.__init__(self, name)
+    self.units = units
+    self.keyword_groups = keyword_groups
+    self.max_dist = max_dist
+  def sticks(self, cluster):
+    cluster.sort(key=lambda t:t['start'])
+    last_token = None
+    for token in cluster:
+      if last_token is not None:
+        dist = _token_dist(last_token, token)
+        if self.max_dist and dist>self.max_dist:
+          return False
+      if token['type'] == 'numeric':
+        continue
+      elif token['type'] == 'unit':
+        if token['value'] not in self.units:
+          return False
+      elif token['type'] == 'keyword':
+        if token['value'] not in self.keyword_groups:
+          return False
+    return True
+
+_STICKY_RULES = [
+  BasicStickyRule('height_imperial', ['ft', 'in'], ['height']),
+  BasicStickyRule('height_metric', ['m', 'cm'], ['height']),
+  BasicStickyRule('weight_imperial', ['st', 'lbs'], ['weight', 'current', 'goal']),
+  BasicStickyRule('weight_metric', ['kg'], ['weight', 'current', 'goal']),
+  BasicStickyRule('delta_imperial', ['st', 'lbs'], ['delta']),
+  BasicStickyRule('delta_metric', ['kg'], ['delta']),
+  BasicStickyRule('bmi', ['bmi'], []),
+]
+_STICKY_RULES = dict((r.name, r) for r in _STICKY_RULES)
+
+def group_tokens(tokens):
+  """
+  Given a list of tokens generated from scrape_numeric, returns a clustering of those tokens
+  by distance, type, and 'stickiness' rules (see above), like this:
+  return [
+    {
+      "rule": "_rule_name_",
+      "tokens": [...],
+    }
+    ...
+  ]
+  """
+  global _STICKY_RULES
+  tokens.sort(key=lambda t:t['start'])
+  clusters = [{'rules':set(_STICKY_RULES.keys()), 'tokens':[t]} for t in tokens]
+  found_merge = True
+  while found_merge:
+    # sort cluster pairs by distance
+    cluster_pairs = []
+    for i in range(len(clusters)-1):
+      c1 = clusters[i]
+      c2 = clusters[i+1]
+      dist = _token_dist(c1['tokens'][-1], c2['tokens'][0])
+      cluster_pairs.append((i, dist))
+    cluster_pairs.sort(key=lambda p:-p[1])
+    # go through the cluster pairs, looking for the clost mergable pair
+    found_merge = False
+    while cluster_pairs:
+      i, dist = cluster_pairs.pop()
+      c1 = clusters[i]
+      c2 = clusters[i+1]
+      rule_intersection = c1['rules'] & c2['rules']
+      if rule_intersection:
+        merged_tokens = c1['tokens']+c2['tokens']
+        merged_rules = set()
+        for rule_name in rule_intersection:
+          rule = _STICKY_RULES[rule_name]
+          if rule.sticks(merged_tokens):
+            found_merge = True
+            merged_rules.add(rule_name)
+        if found_merge:
+          clusters.pop(i+1)
+          clusters[i] = {
+            'rules': merged_rules,
+            'tokens': merged_tokens,
+          }
+          break
+  # select final rules
+  for c in clusters:
+    if len(c['tokens']) == 1:
+      c['rules'] = []
+      c['rule'] = None
+    else:
+      c['rules'] = list(c['rules'])
+      c['rule'] = c['rules'][0]
+  return clusters
--- tests/scraping_tests.py
+++ tests/scraping_tests.py
@@ -1,9 +1,10 @@
 import cry4help as c4h
 
-class TestCase:
-  def __init__(self, string, expected_tokens):
+class TokenizerTest:
+  def __init__(self, string, expected_tokens, unexpected_tokens=None):
     self.string = string
     self.expected_tokens = expected_tokens
+    self.unexpected_tokens = unexpected_tokens if unexpected_tokens else []
   def test(self):
     tokens = c4h.scraping.scrape_numeric(self.string)
     for expected_token in self.expected_tokens:
@@ -19,31 +20,70 @@ class TestCase:
           break
       if not found_match:
         raise RuntimeError("Expected to see token like {} but only got:\n{}".format(expected_token, tokens))
+    for unexpected_token in self.unexpected_tokens:
+      found_match = False
+      for token in tokens:
+        is_match = True
+        for key in expected_token:
+          if token[key] != expected_token[key]:
+            is_match = False
+            break
+        if is_match:
+          found_match = True
+          break
+      if found_match:
+        raise RuntimeError("Expected to not see token like {} but got:\n{}".format(expected_token, tokens))
+
+def clustering_test():
+  tokens = c4h.scraping.scrape_numeric("I've lost 50 lbs and I'm 5' tall")
+  clusters = c4h.scraping.group_tokens(tokens)
+  expected_clusters = [
+    "delta_imperial: ['keyword', 'number', 'unit']",
+    "height_imperial: ['number', 'unit', 'keyword']",
+  ]
+  actual_clusters = []
+  for c in clusters:
+    if c['rule'] is None:
+      continue
+    cluster = "{}: {}".format((c['rule']), [t['type'] for t in c['tokens']])
+    actual_clusters.append(cluster)
+  if "|".join(actual_clusters) != "|".join(expected_clusters):
+    raise RuntimeError("Expected {} but got {}".format(expected_clusters, actual_clusters))
 
 def main():
   TEST_CASES = [
-    TestCase("5 lb", [
+    TokenizerTest("5 lb", [
       {'type':'number', 'value':5, 'start':0, 'end':1},
       {'type':'unit', 'value':'lbs', 'start':2, 'end':4},
     ]),
-    TestCase("I'm worried that I've only lost 56 lbs.", [
+    TokenizerTest("I'm worried that I've only lost 56 lbs.", [
       {'type':'number', 'value':56},
       {'type':'unit', 'value':'lbs'},
     ]),
-    TestCase("I'm worried that I've only lost 27.5 pound.", [
+    TokenizerTest("I'm worried that I've only lost 27.5 pound.", [
       {'type':'number', 'value':27.5},
       {'type':'unit', 'value':'lbs'},
     ]),
-    TestCase("I'm worried that I've only lost .5 st.", [
+    TokenizerTest("I'm worried that I've only lost .5 st.", [
       {'type':'number', 'value':0.5},
       {'type':'unit', 'value':'st'},
     ]),
-    TestCase("I'm worried that I've only lost 10 kgs and my BMI is 50", [
+    TokenizerTest("I'm worried that I've only lost 10 kgs and my BMI is 50", [
       {'type':'number', 'value':10},
       {'type':'unit', 'value':'kg'},
       {'type':'number', 'value':50},
       {'type':'unit', 'value':'bmi'},
     ]),
+    # TODO: improve recognition of feet, meter, and inch units
+    #TokenizerTest("I'm 3m tall.", [
+    #  {'type':'number', 'value':3},
+    #  {'type':'unit', 'value':'m', 'start':5},
+    #], [
+    #  {'type':'unit', 'value':'ft', 'start':1},
+    #  {'type':'unit', 'value':'m', 'start':2},
+    #]),
   ]
   for test_case in TEST_CASES:
     test_case.test()
+  clustering_test()
+
